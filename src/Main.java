5f8eea526716eacc1da4dc78a2286ab654b18d13
Cebix90 <cebulmat@gmail.com>
2023-12-03 00:11:23 +0100

    Checking the correctness of the implemented classes in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        int[] naturalNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        System.out.println(SumArray.sumArray(naturalNumbers));
+
+//        TodoList todoList = new TodoList();
+//        for (int i = 0; i < 10; i++) {
+//            todoList.add("Meeting" + i);
+//        }
+//
+//        todoList.print();
+//
+//        todoList.remove(5);
+//        todoList.remove(5);
+//        todoList.remove(5);
+//
+//        todoList.add("Meeting10");
+//
+//        todoList.print();
+
+
     }
 }
