cdf7e63a6d15131c61cf53b6abf7e76f88be9c41
Cebix90 <cebulmat@gmail.com>
2023-12-05 21:10:31 +0100

    Implemented check for empty or null in CountingWordsInText

--- src/CountingWordsInText.java
+++ src/CountingWordsInText.java
@@ -4,6 +4,10 @@ public class CountingWordsInText {
     public static HashMap<String, Integer> countWords(String text){
         HashMap<String, Integer> hashMap = new HashMap<>();
 
+        if (text == null || text.isEmpty()) {
+            return hashMap;
+        }
+
         String[] words = text.replaceAll("\\p{Punct}", "").toLowerCase().split("\\s+");
 
         for (String word : words) {
